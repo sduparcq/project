ebe1e72f6c9d85dd72f49bfeb5966823db793d74
sduparcq <stephane.duparcq77950@gmail.com>
2026-01-12 11:40:55 +0100

    signal creation momentum

--- momentum.py
+++ momentum.py
@@ -2,8 +2,10 @@
 import pandas as pd
 import Context
 import DataAccesser
-
+from typing import List
 import importlib
+import numpy as np
+
 
 importlib.reload(Context)
 importlib.reload(DataAccesser)
@@ -36,9 +38,38 @@ symbols = df.columns
 n_udl = len(symbols)
 
 period = 24 * 20 # periode de 20 jours
-df['ADA_momentum'] = df['ADA'].pct_change(periods=period)
+
+for symbol in symbols:
+    df[f"{symbol}_momentum"] = df[symbol].pct_change(periods=period)
+
+
+def create_momentum(symbols: List[str], short: int, long: int):
+    for symbol in symbols:
+        df[f"{symbol}_momentum"] = df[symbol].rolling(short).mean() - df[symbol].rolling(long).mean()
+        df[f"{symbol}_momentum"] = (df[f"{symbol}_momentum"] - df[f"{symbol}_momentum"].mean()) / df[f"{symbol}_momentum"].std()
+
+create_momentum(
+    symbols=symbols,
+    short=20,
+    long=100
+)
+
+# %% ANALYSIS
+df.describe()
+
+def create_signal(symbols: List[str]):
+    for symbol in symbols:
+        df[f"{symbol}_signal"] = df[f"{symbol}_momentum"].apply(np.sign)
+
+create_signal(
+    symbols=symbols
+)
 
 df.head(-1)
+
+
+# %% BACKTEST
+
 
 
 
