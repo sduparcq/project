936c12455aba37f82afbd142717b2d4b1a71d4ca
sduparcq <stephane.duparcq77950@gmail.com>
2026-01-12 01:11:48 +0100

    analysis.py

--- analysis.py
+++ analysis.py
@@ -1,7 +1,70 @@
+# %% imports
 import pandas as pd
-from pathlib import Path
+import Context
+import DataAccesser
 
-DATA_PATH = Path(__file__).parent / "data" / "BTC__USDT_1h.parquet"
-df = pd.read_parquet(DATA_PATH)
-print(df.head())
-print(df.columns)
+import importlib
+
+importlib.reload(Context)
+importlib.reload(DataAccesser)
+
+# %% DATA LOADING
+ref_dict = {
+    "ADA": "",
+    "AVAX": "",
+    "BNB": "",
+    "BTC": "",
+    "ETH": "",
+    "LTC": "",
+    "SOL": "",
+    "XRP": ""
+}
+
+for key in list(ref_dict.keys()):
+    ref_dict[key] = f"{key}__USDT"
+
+
+ctx = Context.Context(ref_dict=ref_dict)
+
+ax = DataAccesser.DataAccesser()
+
+df = ax.generate_data_frame(universe=ctx.universe)
+df.head()
+
+# %% ANALYSIS
+
+df.info()
+
+symbols = list(ref_dict.keys())
+n_symbols = len(symbols)
+
+
+
+# %%
+
+norm_df = df.copy()
+
+for col in norm_df.columns:
+    norm_df[col] = (norm_df[col] - norm_df[col].mean()) / norm_df[col].std()
+
+cov_matrix = norm_df.cov()
+
+# %%
+print(cov_matrix)
+
+# %%
+df_save = df.copy()
+
+df_save.head(-1)
+
+
+
+
+
+
+
+
+
+
+
+
